6e54e65523420f2822ebbe88988f20703d72779c
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-21 08:43:42 +0200

    Made it possible to specify feeds with environment variables

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -12,11 +12,18 @@ defmodule RssRouter.Supervisor do
   end
 
   def start_initial_feeds() do
-    RssRouter.FeedStore.get_feeds()
+    get_initial_feeds()
     |> Enum.map(fn feed ->
       {RssRouter.FeedService,
        %RssRouter.RoutingRule{uri: feed, publisher: RssRouter.PocketPublisher}}
     end)
     |> Enum.each(fn child -> DynamicSupervisor.start_child(RssRouter.Supervisor, child) end)
+  end
+
+  defp get_initial_feeds() do
+    (System.get_env("FEEDS") || "")
+    |> String.split(" ")
+    |> Enum.concat(RssRouter.FeedStore.get_feeds())
+    |> Enum.reject(fn f -> String.length(f) == 0 end)
   end
 end

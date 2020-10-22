556afab407e355a69037db41ad021f0e9e86dafe
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-22 21:19:17 +0200

    Small refactor of feed_processor

--- lib/rss_router/processor/feed_processor.ex
+++ lib/rss_router/processor/feed_processor.ex
@@ -2,17 +2,17 @@ defmodule RssRouter.FeedProcessor do
   def process_feed(rule) do
     feed = RssRouter.FeedReader.read_feed(rule.uri)
 
-    get_new_feed_entries(feed)
-    |> Enum.each(fn entry -> process_feed_entry(rule, entry) end)
+    process_new_feeds(feed, rule)
 
     update_latest_timestamp(feed)
   end
 
-  defp get_new_feed_entries(feed) do
+  defp process_new_feeds(feed, rule) do
     latest_timestamp = RssRouter.FeedStore.get_feed_latest_timestamp(feed.title)
 
     feed.entries
     |> Enum.filter(fn e -> feed_entry_is_new(latest_timestamp, e.updated) end)
+    |> Enum.each(fn entry -> process_feed_entry(rule, entry) end)
   end
 
   defp update_latest_timestamp(feed) do

9a17ceb0025bfdfaf573cc1fae9dd41bac918549
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-23 15:15:42 +0200

    Added basic auth

--- lib/web/endpoint.ex
+++ lib/web/endpoint.ex
@@ -1,5 +1,7 @@
 defmodule RssRouter.Web.Endpoint do
   use Phoenix.Endpoint, otp_app: :rss_router
+  import Plug.BasicAuth
 
+  plug(:basic_auth, username: "hello", password: "secret")
   plug(RssRouter.Web.Router)
 end

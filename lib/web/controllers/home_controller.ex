dfdbbff82c848cf473de18bba13b8c66e2948adf
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-26 21:37:49 +0100

    Show all feeds on home page

--- lib/web/controllers/home_controller.ex
+++ lib/web/controllers/home_controller.ex
@@ -2,6 +2,7 @@ defmodule RssRouter.Web.HomeController do
   use RssRouter.Web, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    feeds = RssRouter.FeedStore.get_feeds()
+    render(conn, "index.html", feeds: feeds)
   end
 end

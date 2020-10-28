2ab2075635daacc2b255cf02243ba002d7b50ee6
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-28 10:25:28 +0100

    Refactored FeedStore

--- lib/data_store/feed_store.ex
+++ lib/data_store/feed_store.ex
@@ -1,73 +1,61 @@
 defmodule RssRouter.FeedStore do
   @spec get_feeds() :: [String.t()]
   def get_feeds() do
-    {:ok, table} = get_or_create_table!()
-
-    try do
+    query(fn table ->
       get_feeds(table)
-    after
-      close(table)
-    end
+    end)
   end
 
   @spec insert_feed(Strint.t()) :: :ok
   def insert_feed(feed) do
-    {:ok, table} = get_or_create_table!()
-
-    try do
+    query(fn table ->
       feeds = Enum.uniq(get_feeds(table) ++ [feed])
       :ok = :dets.insert(table, {:feeds, feeds})
-    after
-      close(table)
-    end
+    end)
   end
 
   @spec get_feed_latest_timestamp(String.t()) :: DateTime | :none
   def get_feed_latest_timestamp(feed_title) do
-    {:ok, table} = get_or_create_table!()
-
-    try do
+    query(fn table ->
       result = :dets.lookup(table, feed_title)
 
       case result do
         [{^feed_title, timestamp}] -> timestamp
         [] -> :none
       end
-    after
-      close(table)
-    end
+    end)
   end
 
   @spec set_feed_latest_timestamp(String.t(), DateTime) :: :ok
   def set_feed_latest_timestamp(feed_title, timestamp) do
-    {:ok, table} = get_or_create_table!()
-
-    try do
+    query(fn table ->
       :ok = :dets.insert(table, {feed_title, timestamp})
-    after
-      close(table)
-    end
+    end)
   end
 
   def delete_latest_timestamp(feed_title) do
-    {:ok, table} = get_or_create_table!()
-
-    try do
+    query(fn table ->
       :ok = :dets.delete(table, feed_title)
-    after
-      close(table)
-    end
+    end)
   end
 
   @spec get_all_latest_entries() :: [{String.t(), DateTime}]
   def get_all_latest_entries() do
-    {:ok, table} = get_or_create_table!()
-
-    try do
+    query(fn table ->
       :dets.foldl(fn x, res -> res ++ [x] end, [], table)
       |> Enum.reject(fn {x, _} -> x == :feeds end)
+    end)
+  end
+
+  defp query(fun) do
+    File.mkdir_p(RssRouter.Config.data_path())
+    dets_file = to_charlist(RssRouter.Config.data_path()) ++ '/feed_data'
+    {:ok, table} = :dets.open_file(dets_file, access: :read_write, type: :set)
+
+    try do
+      fun.(table)
     after
-      close(table)
+      :dets.close(table)
     end
   end
 
@@ -78,18 +66,5 @@ defmodule RssRouter.FeedStore do
       [feeds: feeds] -> feeds
       [] -> []
     end
-  end
-
-  defp get_or_create_table!() do
-    File.mkdir_p(RssRouter.Config.data_path())
-    :dets.open_file(dets_file(), access: :read_write, type: :set)
-  end
-
-  defp dets_file() do
-    to_charlist(RssRouter.Config.data_path()) ++ '/feed_data'
-  end
-
-  defp close(table) do
-    :dets.close(table)
   end
 end

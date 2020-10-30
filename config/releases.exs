a7e546881b36271d25b4ae891dc9a969bbe9c16e
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-30 21:06:58 +0100

    Made port fixed, as it can be mapped with docker

--- config/releases.exs
+++ config/releases.exs
@@ -2,6 +2,6 @@ import Config
 
 config :rss_router, RssRouter.Web.Endpoint,
   url: [host: System.fetch_env!("RSS_ROUTER_HOST")],
-  http: [port: System.fetch_env!("RSS_ROUTER_PORT")],
+  http: [port: 4000],
   secret_key_base: System.fetch_env!("RSS_ROUTER_SECRET_KEY_BASE"),
   server: true

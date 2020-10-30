ae33b7370c26ed9a8184972618decff831962e41
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-30 22:09:14 +0100

    Made base path configurable

--- config/releases.exs
+++ config/releases.exs
@@ -1,7 +1,10 @@
 import Config
 
 config :rss_router, RssRouter.Web.Endpoint,
-  url: [host: System.fetch_env!("RSS_ROUTER_HOST")],
+  url: [
+    host: System.fetch_env!("RSS_ROUTER_HOST"),
+    path: System.get_env("RSS_ROUTER_BASE_PATH" || "/")
+  ],
   http: [port: 4000],
   secret_key_base: System.fetch_env!("RSS_ROUTER_SECRET_KEY_BASE"),
   server: true

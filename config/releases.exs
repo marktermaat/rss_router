e03607ba9c342ee9709ba58944a4031a04be9d9d
Mark ter Maat <livestockmanagement@nedap.com>
2020-10-30 22:23:26 +0100

    Made base path obligatory

--- config/releases.exs
+++ config/releases.exs
@@ -3,7 +3,7 @@ import Config
 config :rss_router, RssRouter.Web.Endpoint,
   url: [
     host: System.fetch_env!("RSS_ROUTER_HOST"),
-    path: System.get_env("RSS_ROUTER_BASE_PATH") || "/"
+    path: System.fetch_env!("RSS_ROUTER_BASE_PATH")
   ],
   http: [port: 4000],
   secret_key_base: System.fetch_env!("RSS_ROUTER_SECRET_KEY_BASE"),

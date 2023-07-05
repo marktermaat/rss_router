520356bde8524c3daf5cf8d9238ff939952ab112
Mark ter Maat <mark.termaat@nedap.com>
2023-07-05 20:18:48 +0200

    Increased docker version to 0.3 for ARM support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 docker build -t marktermaat/rss_router -f docker/Dockerfile .
-docker tag marktermaat/rss_router marktermaat/rss_router:0.2
-docker push marktermaat/rss_router:0.2
+docker tag marktermaat/rss_router marktermaat/rss_router:0.3
+docker push marktermaat/rss_router:0.3
 docker push marktermaat/rss_router:latest
